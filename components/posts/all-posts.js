2455732cf15aebf01501b6548f924e04378e7724
tburakonat <tburakonat@gmail.com>
2022-12-19 13:21:07 +0100

    fill all posts page with dummy data

---
+++ components/posts/all-posts.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/all-posts.module.css';
+import PostGrid from './post-grid';
+
+export default function AllPosts(props) {
+	return (
+		<section className={styles.posts}>
+			<h1>All Posts</h1>
+			<PostGrid posts={props.posts} />
+		</section>
+	);
+}
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,40 @@
-import React from 'react';
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+	{
+		title: 'Getting Started with Next.js',
+		image: 'getting-started-nextjs.png',
+		excerpt:
+			'Next.js is the React Framework for Production. It makes building fullstack React apps and sites a breeze.',
+		date: '2022-02-10',
+		slug: 'getting-started-with-nextjs',
+	},
+	{
+		title: 'Getting Started with Next.js',
+		image: 'getting-started-nextjs.png',
+		excerpt:
+			'Next.js is the React Framework for Production. It makes building fullstack React apps and sites a breeze.',
+		date: '2022-02-10',
+		slug: 'getting-started-with-nextjs2',
+	},
+	{
+		title: 'Getting Started with Next.js',
+		image: 'getting-started-nextjs.png',
+		excerpt:
+			'Next.js is the React Framework for Production. It makes building fullstack React apps and sites a breeze.',
+		date: '2022-02-10',
+		slug: 'getting-started-with-nextjs3',
+	},
+	{
+		title: 'Getting Started with Next.js',
+		image: 'getting-started-nextjs.png',
+		excerpt:
+			'Next.js is the React Framework for Production. It makes building fullstack React apps and sites a breeze.',
+		date: '2022-02-10',
+		slug: 'getting-started-with-nextjs4',
+	},
+];
 
 export default function AllPostsPage() {
-	return <div>AllPostsPage</div>;
+	return <AllPosts posts={DUMMY_POSTS} />;
 }
