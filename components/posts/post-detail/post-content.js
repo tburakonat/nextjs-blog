5aa6736dfef5b6abd640390f5dde77e128366926
tburakonat <tburakonat@gmail.com>
2022-12-19 23:10:20 +0100

    remove unnecessary dependencies

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,7 +3,10 @@ import styles from '../../../styles/post-content.module.css';
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
+import js from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
+
+SyntaxHighlighter.registerLanguage('js', js);
 
 export default function PostContent(props) {
 	const { post } = props;
